d1a4305d4bd88f3434048ddc1bcfec72f00f0aa4
KyoYasuo <kyoyasuo221@gmail.com>
2023-11-21 00:05:36 +0900

    update DTO

--- API/CodePulse.API/CodePulse.API/Models/DTO/CategoryDto.cs
+++ API/CodePulse.API/CodePulse.API/Models/DTO/CategoryDto.cs
@@ -1,7 +1,8 @@
 ﻿namespace CodePulse.API.Models.DTO
 {
-    public class CreateCategoryRequestDto
+    public class CategoryDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string UrlHandle { get; set; }
     }
